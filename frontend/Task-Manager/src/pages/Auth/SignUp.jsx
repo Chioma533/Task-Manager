a39c3b9fc6862088b8caf13b52a917161727a7d3
Chioma533 <queenmichael527@gmail.com>
2026-04-11 19:15:21 +0100

    integrated signup API and fixed signup route in backend

--- frontend/Task-Manager/src/pages/Auth/SignUp.jsx
+++ frontend/Task-Manager/src/pages/Auth/SignUp.jsx
@@ -1,9 +1,13 @@
-import React, { useState } from 'react'
+import React, { useContext, useState } from 'react'
 import AuthLayout from '../../components/layout/AuthLayout'
 import { validateEmail } from '../../utils/helper'
 import ProfilePhotoSelector from '../../components/inputs/ProfilePhotoSelector'
 import Input from '../../components/inputs/Input'
-import { Link } from 'react-router-dom'
+import { Link, useNavigate } from 'react-router-dom'
+import axiosInstance from '../../utils/axiosInstance'
+import { API_PATHS } from '../../utils/apiPaths'
+import { UserContext } from '../../context/userContext'
+import uploadImage from '../../utils/uploadImage'
 
 const SignUp = () => {
   const [profilePic, setProfilePic] = useState(null)
@@ -14,9 +18,14 @@ const SignUp = () => {
 
   const [error, setError] = useState(null)
 
+  const { updateUser } = useContext(UserContext)
+  const navigate = useNavigate()
+
   // Handle signup form submit
   const handleSignUp = async (e) => {
     e.preventDefault()
+
+    let profileImageUrl = "";
 
     if (!fullName) {
       setError("Please enter your full name")
@@ -35,7 +44,46 @@ const SignUp = () => {
     setError("")
 
     // Signup API call
-    
+
+    try {
+      // Upload image if available
+      if (profilePic) {
+        const imgUploadRes = await uploadImage(profilePic)
+        profileImageUrl = imgUploadRes.imageUrl || "";
+      }
+
+
+      const response = await axiosInstance.post(API_PATHS.AUTH.REGISTER, {
+        name: fullName,
+        email,
+        password,
+        profileImageUrl,
+        adminInviteToken
+      })
+
+      const { token, role } = response.data;
+
+      if (token) {
+        localStorage.setItem("token", token)
+        updateUser(response.data)
+
+        // redirect based on role
+        if (role === "admin") {
+          navigate("/admin/dashboard")
+        } else {
+          navigate("/user/dashboard")
+        }
+      }
+
+    } catch (error) {
+      console.log("FULL ERROR:", error); // 👈 ADD THIS
+
+      if (error.response && error.response.data.message) {
+        setError(error.response.data.message)
+      } else {
+        setError("Something went wrong. Please try again")
+      }
+    }
 
   }
   return (
@@ -70,21 +118,21 @@ const SignUp = () => {
               type="password"
             />
             <Input
-              value={password}
-              onChange={({ target }) => setPassword(target.value)}
+              value={adminInviteToken}
+              onChange={({ target }) => setAdminInviteToken(target.value)}
               label='Admin Invite Token'
               placeholder='6 Digit Code'
               type="text"
             />
 
           </div>
-            {error && <p className='text-red-500 text-xs pb-2.5'>{error}</p>}
+          {error && <p className='text-red-500 text-xs pb-2.5'>{error}</p>}
 
-            <button type='submit' className='btn-primary'>SIGNUP</button>
-            <p className="text-[13px] text-slate-800 mt-3">
-              Already have an account? {" "}
-              <Link className='font-medium text-primary underline' to='/login'>LogIn</Link>
-            </p>
+          <button type='submit' className='btn-primary'>SIGNUP</button>
+          <p className="text-[13px] text-slate-800 mt-3">
+            Already have an account? {" "}
+            <Link className='font-medium text-primary underline' to='/login'>LogIn</Link>
+          </p>
         </form>
       </div>
 
